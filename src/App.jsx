5737f6f266fa0a21a5fb0fb6f7f18ef802239d9a
xaphaniafox <f.hosseini64@gmail.com>
2023-09-03 06:31:07 +0300

    register component ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import Login from "./features/identity/components/login";
+// import Login from "./features/identity/components/login";
+import Register from "./features/identity/components/register";
 function App() {
-  return <Login />;
+  return <Register />;
 }
 
 export default App;
