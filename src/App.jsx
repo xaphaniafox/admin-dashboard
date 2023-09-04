9c02e7d6aeaa5ff15bfa2d4d3d470e6b42f5b237
xaphaniafox <f.hosseini64@gmail.com>
2023-09-04 09:14:44 +0300

    creating routes for login and register

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-// import Login from "./features/identity/components/login";
-import Register from "./features/identity/components/register";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 function App() {
-  return <Register />;
+  return <RouterProvider router={router} />;
 }
-
 export default App;
