629601d7cd8d1b47b425402aac1f614b945f4e6f
xaphaniafox <f.hosseini64@gmail.com>
2023-09-04 11:32:23 +0300

    useForm and form validation

--- src/features/identity/components/register.jsx
+++ src/features/identity/components/register.jsx
@@ -1,6 +1,16 @@
 import logo from "@assets/images/logo.svg";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <>
       <div className="text-center mt-4">
@@ -19,24 +29,77 @@ const Register = () => {
       <div className="card">
         <div className="card-body">
           <div className="m-sm-4">
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="mb-3">
                 <label className="form-label">موبایل</label>
-                <input className="form-control form-control-lg" />
+                <input
+                  {...register("mobile", {
+                    required: "موبایل الزامی است",
+                    minLength: 11,
+                    maxLength: 11,
+                  })}
+                  className={`form-control form-control-lg ${
+                    errors.mobile && "is-invalid"
+                  }`}
+                />
+                {errors.mobile && errors.mobile.type === "required" && (
+                  <p className="text-danger small fw-bolder mt-1">
+                    {errors.mobile?.message}
+                  </p>
+                )}
+                {errors.mobile &&
+                  (errors.mobile.type === "minLength" ||
+                    errors.mobile.type === "maxLength") && (
+                    <p className="text-danger small fw-bolder mt-1">
+                      موبایل باید 11 کاراکتر باشد
+                    </p>
+                  )}
               </div>
               <div className="mb-3">
                 <label className="form-label">رمز عبور</label>
                 <input
-                  className="form-control form-control-lg mb-2"
+                  {...register("password", {
+                    required: "رمز عبور الزامی است",
+                  })}
+                  className={`form-control form-control-lg mb-2 ${
+                    errors.password && "is-invalid"
+                  }`}
                   type="password"
                 />
+                {errors.password && (
+                  <p className="text-danger small fw-bolder mt-1">
+                    {errors.password?.message}
+                  </p>
+                )}
               </div>
               <div className="mb-3">
                 <label className="form-label">تکرار رمز عبور</label>
                 <input
-                  className="form-control form-control-lg mb-2"
+                  {...register("confirmPassword", {
+                    required: "تکرار رمز عبور الزامی است",
+                    validate: (value) => {
+                      if (watch("password") !== value) {
+                        return "عدم تطابق با رمز عبور";
+                      }
+                    },
+                  })}
+                  className={`form-control form-control-lg mb-2 ${
+                    errors.confirmPassword && "is-invalid"
+                  }`}
                   type="password"
                 />
+                {errors.confirmPassword &&
+                  errors.confirmPassword.type === "required" && (
+                    <p className="text-danger small fw-bolder mt-1">
+                      {errors.confirmPassword?.message}
+                    </p>
+                  )}
+                {errors.confirmPassword &&
+                  errors.confirmPassword.type === "validate" && (
+                    <p className="text-danger small fw-bolder mt-1">
+                      {errors.confirmPassword?.message}
+                    </p>
+                  )}
               </div>
               <div className="text-center mt-3">
                 <button type="submit" className="btn btn-lg btn-primary">
